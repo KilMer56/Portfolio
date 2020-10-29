a21ff593bb7a5f538c289895bca47b69490c05f3
KilMer56 <mer.killian56@gmail.com>
2020-10-29 22:04:14 +0100

    Clean + add imagesé

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import '../assets/css/App.css';
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p class="font-raleway font-bold">Portfolio</p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
