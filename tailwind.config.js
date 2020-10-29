3c448c63f60e032bcb9e6ccf51085b8fedf16945
KilMer56 <mer.killian56@gmail.com>
2020-10-29 21:52:31 +0100

    Add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway'],
+        quicksand: ['Quicksand']
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
