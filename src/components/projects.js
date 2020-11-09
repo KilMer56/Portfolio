5dacdfb1c5b8d63d6010e1a5a93cc79ec7691a3e
KilMer56 <mer.killian56@gmail.com>
2020-11-09 15:08:08 +0100

    Responsive : More + contact

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,11 +4,13 @@ import projects from "../assets/json/projects.json"
 function renderProjectCards(projects, printDetails=false){
     return projects.map((data) => {
         return ( 
-            <div class="w-full md:w-1/2 lg:w-1/3 mb-4 md:mr-4 p-5 bg-dark cursor-pointer transition-shadow duration-300 ease-in-out hover:shadow-2xl">
-                <p class="font-bold text-xl">{data.title}</p>
-                {printDetails && <p class="mt-2"><span class="mr-4">{data.type}</span><span class="text-grey">{data.dates}</span></p>}
-                <p class="text-grey-2 py-4">{data.description}</p>
-                <p class="text-grey break-words">{data.technologies.map((data) => <span class="mr-4">{data}</span>)}</p>
+            <div class="w-full md:w-1/2 lg:w-1/3 mb-4 md:pr-4">
+                <div class="h-full p-5 bg-dark cursor-pointer transition-shadow duration-300 ease-in-out hover:shadow-2xl">
+                    <p class="font-bold text-xl">{data.title}</p>
+                    {printDetails && <p class="mt-2"><span class="mr-4">{data.type}</span><span class="text-grey">{data.dates}</span></p>}
+                    <p class="text-grey-2 py-4">{data.description}</p>
+                    <p class="text-grey break-words">{data.technologies.map((data) => <span class="mr-4">{data}</span>)}</p>
+                </div>
             </div>
         )
     })
@@ -20,10 +22,10 @@ function Projects(){
             <Header title="Projects"/>
             <div class="p-big font-raleway text-primary">
                 <p class="font-bold text-3xl text-center mb-8">Key projects</p>
-                <div class="flex flex-col md:flex-row justify-between py-4">
+                <div class="flex md:flex-wrap justify-center flex-col md:flex-row py-4">
                     {renderProjectCards(projects.big, true)}
                 </div>
-                <p class="font-bold text-3xl text-center m-8">Small personal projects</p>
+                <p class="font-bold md:flex-wrap text-3xl text-center m-8">Small personal projects</p>
                 <div class="flex flex-col md:flex-row justify-between py-4">
                     {renderProjectCards(projects.small)}
                 </div>
